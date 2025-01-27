b71295aae1b266f75b54ebdb7a329a22e92993b7
Amirali Zabihi <108997237+azbihi@users.noreply.github.com>
2025-01-27 17:08:15 +0330

    some struct const variables aded

--- core/main.cpp
+++ core/main.cpp
@@ -1,2 +1,24 @@
 #include <iostream>
 using namespace std;
+
+//Constants
+//intruducing a couple of const Variables for size of array
+const int MAX_STUDENTS = 100 ;
+const int MAX_COURSES = 10 ;
+ //Structs
+ // intruducing a couple of structs to keeping our students data
+ struct Course {
+     string name;
+     int units;
+     float grade;
+ };
+
+ struct Student {
+     string firstName;
+     string lastName;
+     string studentId;
+     string major;
+     Course courses[MAX_COURSES];
+     int courseCount;
+     float gpa;
+ };
